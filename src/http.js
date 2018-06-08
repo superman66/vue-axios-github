1ef56368901533bb6a49761f32c940bff612669f
Superman <supermanchc@gmail.com>
2018-06-08 22:49:33 +0800

    fixed: 多个请求 401 的情况，重定向到登录页面 query 参数错误

--- src/http.js
+++ src/http.js
@@ -9,40 +9,45 @@ import * as types from './store/types'
 import router from './router'
 
 // axios 配置
-axios.defaults.timeout = 5000;
-axios.defaults.baseURL = 'https://api.github.com';
+axios.defaults.timeout = 5000
+axios.defaults.baseURL = 'https://api.github.com'
 
 // http request 拦截器
 axios.interceptors.request.use(
-    config => {
-        if (store.state.token) {
-            config.headers.Authorization = `token ${store.state.token}`;
-        }
-        return config;
-    },
-    err => {
-        return Promise.reject(err);
-    });
+  config => {
+    if (store.state.token) {
+      config.headers.Authorization = `token ${store.state.token}`
+    }
+    return config
+  },
+  err => {
+    return Promise.reject(err)
+  },
+)
 
 // http response 拦截器
 axios.interceptors.response.use(
-    response => {
-        return response;
-    },
-    error => {
-        if (error.response) {
-            switch (error.response.status) {
-                case 401:
-                    // 401 清除token信息并跳转到登录页面
-                    store.commit(types.LOGOUT);
-                    router.replace({
-                        path: 'login',
-                        query: {redirect: router.currentRoute.fullPath}
-                    })
-            }
-        }
-        // console.log(JSON.stringify(error));//console : Error: Request failed with status code 402
-        return Promise.reject(error.response.data)
-    });
+  response => {
+    return response
+  },
+  error => {
+    if (error.response) {
+      switch (error.response.status) {
+        case 401:
+          // 401 清除token信息并跳转到登录页面
+          store.commit(types.LOGOUT)
+          
+          // 只有在当前路由不是登录页面才跳转
+          router.currentRoute.path !== 'login' &&
+            router.replace({
+              path: 'login',
+              query: { redirect: router.currentRoute.path },
+            })
+      }
+    }
+    // console.log(JSON.stringify(error));//console : Error: Request failed with status code 402
+    return Promise.reject(error.response.data)
+  },
+)
 
-export default axios;
+export default axios
